604d5bbbf6647f14a12d1b0242e006c574f6fc5b
Pei Wu <pwu@tango.me>
2016-04-05 18:14:15 -0700

    Initial Commit

---
+++ LabelWithSamelineRangeConstrains/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LabelWithSamelineRangeConstrains
+//
+//  Created by Pei Wu on 4/5/16.
+//  Copyright © 2016 Pei Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LabelWithSamelineRangeConstrains/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LabelWithSamelineRangeConstrains
+//
+//  Created by Pei Wu on 4/5/16.
+//  Copyright © 2016 Pei Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
