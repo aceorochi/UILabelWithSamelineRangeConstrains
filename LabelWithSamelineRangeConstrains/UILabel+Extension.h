23206c0b03eca10beeba0bdd1edb7f2181f55138
Pei Wu <pwu@tango.me>
2016-04-05 18:33:10 -0700

    first commit

---
+++ LabelWithSamelineRangeConstrains/UILabel+Extension.h
@@ -0,0 +1,13 @@
+//
+//  UILabel+Extension.h
+//  LabelWithSamelineRangeConstrains
+//
+//  Created by Pei Wu on 4/5/16.
+//  Copyright © 2016 Pei Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (Extension)
+
+@end
---
+++ LabelWithSamelineRangeConstrains/UILabel+Extension.m
@@ -0,0 +1,13 @@
+//
+//  UILabel+Extension.m
+//  LabelWithSamelineRangeConstrains
+//
+//  Created by Pei Wu on 4/5/16.
+//  Copyright © 2016 Pei Wu. All rights reserved.
+//
+
+#import "UILabel+Extension.h"
+
+@implementation UILabel (Extension)
+
+@end
